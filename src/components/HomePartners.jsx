3bc4f1a213da3f0b73b740be94bb34b592d02a59
Hardik Shah <58135993+HardikShah563@users.noreply.github.com>
2024-05-04 00:46:01 +0530

    home partners

--- src/components/HomePartners.jsx
+++ src/components/HomePartners.jsx
@@ -1,13 +1,36 @@
+// importing from react
+import { Link } from "react-router-dom";
+// importing data
+import data from "../data.json";
+
 export default function HomePartners() {
+    const partners = data.home.homePartners;
+
     return (
         <>
             <div className="w-90 margin-a-a margin-block-50">
-                <h1 className="small-heading gray-text">
-                    Our Supporters
+                <h1 className="small-heading txt-ctr gray-text margin-block-50">
+                    {partners.title}
                 </h1>
 
-                <div className="">
-                    {/* {} */}
+                <div className="flex justify-evenly flex-wrap">
+                    {partners.logos.map((logo) => (
+                        <div className="padding-10">
+                            <img
+                                src={
+                                    process.env.PUBLIC_URL
+                                    + "/assets/logos/"
+                                    + logo
+                                }
+                                alt={logo}
+                                className="home-partners"
+                            />
+                        </div>
+                    ))}
+                </div>
+
+                <div className="flex justify-ctr margin-block-50">
+                    <Link onClick={() => { window.scrollTo(0, 0); }} className="light-button" to="/partners&supporters">View All Our Supporters</Link>
                 </div>
             </div>
         </>
